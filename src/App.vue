ba636a595521f887851067abdf6f3cb8f180bd2d
Alana-Rocha <alanarodrigues.rocha1@gmail.com>
2025-02-03 17:25:49 -0300

    adiciona template inicial para projeto Vue 3 com Vite e Tailwind CSS

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Card from './components/Card.vue';
+</script>
+
+<template>
+  <header>teste</header>
+
+  <Card imagem="https://via.placeholder.com/300" titulo="Meu Card"
+    descricao="Esta é uma descrição de exemplo para o card." :tags="['fotografia', 'viagem', 'inverno']" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="max-w-sm rounded overflow-hidden shadow-lg card">
+    <img class="w-full" :src="imagem" :alt="titulo" />
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">{{ titulo }}</div>
+      <p class="text-gray-700 text-base">{{ descricao }}</p>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      <span v-for="tag in tags" :key="tag"
+        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+        {{ tag }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    imagem: String,
+    titulo: String,
+    descricao: String,
+    tags: Array
+  }
+};
+
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  max-width: 300px;
+}
+</style>
