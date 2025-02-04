6915f2b23df9c7d8164070dba68dccf1cfd2469e
Alana-Rocha <alanarodrigues.rocha1@gmail.com>
2025-02-04 15:46:41 -0300

    fix: adiciona componente Button com suporte a classes dinâmicas e remove componente Input

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+    <button :class="computedClass" @click="$emit('click')">
+        {{ label }}
+    </button>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    label: {
+        type: String,
+        default: 'Button'
+    },
+    primary: {
+    type: Boolean,
+    default: true
+}
+  });
+
+const computedClass = computed(() => {
+    return props.primary
+        ? 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'
+        : 'bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full';
+});
+</script>
